94399b0c20683b7a494037db2116f79de7d81fcc
mccalluc <mccalluc@users.noreply.github.com>
2023-04-15 23:05:35 -0400

    add fingering tests

--- tests/test_layouts.py
+++ tests/test_layouts.py
@@ -16,3 +16,20 @@ def test_UnisonoricLayout_str():
     assert str(u_layout) == \
     'C4  E4  G4      C5  E5  G5 \n' \
     'G4  B4  D5      G5  B5  D6 '
+
+u_fingering = UnisonoricFingering(
+    u_layout,
+    [[False, False, True],
+     [True, False, False]],
+    [[False, False, True],
+     [True, False, False]])
+
+def test_UnisonoricFingering_repr():
+    r = repr(u_fingering)
+    assert "UnisonoricFingering(UnisonoricLayout([[Pitch(value=48, name='C4')" in r
+    assert "[[False, False, True], [True, False, False]], [[False, False, True], [True, False, False]])" in r
+
+def test_UnisonoricFingering_str():
+    assert str(u_fingering) == \
+    '--- --- G4      --- --- G5 \n' \
+    'G4  --- ---     G5  --- ---'
