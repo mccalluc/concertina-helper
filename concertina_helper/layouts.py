3ede54154d54da227d8135126e2e31b52845c7fd
mccalluc <mccalluc@users.noreply.github.com>
2023-04-18 22:10:15 -0400

    group tests; layout.shape

--- concertina_helper/layouts.py
+++ concertina_helper/layouts.py
@@ -9,6 +9,8 @@ class Direction(Enum):
 
 
 Mask = list[list[bool]]
+Shape = tuple[list[int], list[int]]
+PitchMatrix = list[list[Pitch]]
 
 
 class UnisonoricFingering:
@@ -52,9 +54,16 @@ class BisonoricFingering:
 
 
 class UnisonoricLayout:
-    def __init__(self, left: list[list[Pitch]], right: list[list[Pitch]]):
+    def __init__(self, left: PitchMatrix, right: PitchMatrix):
         self.left = left
         self.right = right
+    
+    @property
+    def shape(self) -> Shape:
+        return (
+            [len(row) for row in self.left],
+            [len(row) for row in self.right],
+        )
 
     def _get_masks(self) -> tuple[Mask, Mask]:
         return (
@@ -98,8 +107,18 @@ class UnisonoricLayout:
 
 class BisonoricLayout:
     def __init__(self, push_layout: UnisonoricLayout, pull_layout: UnisonoricLayout):
+        if push_layout.shape != pull_layout.shape:
+            raise Exception(
+                f'Push and pull layout shapes must match: {push_layout.shape} != {pull_layout.shape}')
         self.push_layout = push_layout
         self.pull_layout = pull_layout
+    
+    @property
+    def shape(self) -> Shape:
+        return (
+            [len(row) for row in self.push_layout.left],
+            [len(row) for row in self.push_layout.right],
+        )
 
     def get_fingerings(self, pitch: Pitch) -> set[BisonoricFingering]:
         push_fingerings = self.push_layout.get_fingerings(pitch)
@@ -117,7 +136,7 @@ class BisonoricLayout:
             f'{Direction.PULL.name}:\n{self.pull_layout}'
 
 
-def _names_to_pitches(matrix: list[list[str]]) -> list[list[Pitch]]:
+def _names_to_pitches(matrix: list[list[str]]) -> PitchMatrix:
     '''
     >>> pitch_matrix = _names_to_pitches([['C4']])
     >>> pitch_matrix[0][0].name
@@ -125,30 +144,25 @@ def _names_to_pitches(matrix: list[list[str]]) -> list[list[Pitch]]:
     '''
     return [[Pitch.from_name(name) for name in row] for row in matrix]
 
-
-__cg_anglo_wheatstone_push_layout = UnisonoricLayout(
-    _names_to_pitches(
-        [['E3', 'A3', 'C#4', 'A4', 'G#4'],
-         ['C3', 'G3', 'C4', 'E4', 'G4'],
-            ['B3', 'D4', 'G4', 'B4', 'D5']]),
-    _names_to_pitches(
-        [['C#5', 'A5', 'G#5', 'C#6', 'A6'],
-         ['C5', 'E5', 'G5', 'C6', 'E6'],
-            ['G5', 'B5', 'D6', 'G6', 'B6']]),
-)
-
-__cg_anglo_wheatstone_pull_layout = UnisonoricLayout(
-    _names_to_pitches(
-        [['F3', 'Bb3', 'D#4', 'G4', 'Bb4'],
-         ['G3', 'B3', 'D4', 'F4', 'A4'],
-            ['A3', 'F#4', 'A4', 'C5', 'E5']]),
-    _names_to_pitches(
-        [['D#5', 'G5', 'Bb5', 'D#6', 'F6'],
-         ['B4', 'D5', 'F5', 'A5', 'B5'],
-            ['F#5', 'A5', 'C6', 'E6', 'F#6']])
-)
-
 cg_anglo_wheatstone_layout = BisonoricLayout(
-    push_layout=__cg_anglo_wheatstone_push_layout,
-    pull_layout=__cg_anglo_wheatstone_pull_layout,
+    push_layout=UnisonoricLayout(
+        _names_to_pitches(
+            [['E3', 'A3', 'C#4', 'A4', 'G#4'],
+             ['C3', 'G3', 'C4', 'E4', 'G4'],
+                ['B3', 'D4', 'G4', 'B4', 'D5']]),
+        _names_to_pitches(
+            [['C#5', 'A5', 'G#5', 'C#6', 'A6'],
+             ['C5', 'E5', 'G5', 'C6', 'E6'],
+                ['G5', 'B5', 'D6', 'G6', 'B6']]),
+    ),
+    pull_layout=UnisonoricLayout(
+        _names_to_pitches(
+            [['F3', 'Bb3', 'D#4', 'G4', 'Bb4'],
+             ['G3', 'B3', 'D4', 'F4', 'A4'],
+                ['A3', 'F#4', 'A4', 'C5', 'E5']]),
+        _names_to_pitches(
+            [['D#5', 'G5', 'Bb5', 'D#6', 'F6'],
+             ['B4', 'D5', 'F5', 'A5', 'B5'],
+                ['F#5', 'A5', 'C6', 'E6', 'F#6']])
+    ),
 )
