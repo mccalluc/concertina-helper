ce50809b01994d0629374a3159757a33a63f0703
mccalluc <mccalluc@users.noreply.github.com>
2023-04-21 12:34:03 -0400

    --verbose

--- concertina_helper/from_abc.py
+++ concertina_helper/from_abc.py
@@ -28,14 +28,18 @@ prints possible fingerings.
         '--layout', choices=['Wheatstone'], default='Wheatstone')
     parser.add_argument(
         '--key', choices=['CG'], default='CG')
+    parser.add_argument(
+        '--verbose', action='store_true'
+    )
 
     args = parser.parse_args()
     path = Path(args.abc)
     tune = Tune(path.read_text())
-    print_fingerings(tune)
+    verbose = args.verbose
+    print_fingerings(tune, verbose)
 
 
-def print_fingerings(tune: Tune):
+def print_fingerings(tune: Tune, verbose: bool):
     for i, measure in enumerate(tune.measures):
         print(f'Measure {i + 1}')
         for note in measure:
@@ -43,4 +47,4 @@ def print_fingerings(tune: Tune):
             print(indent(pitch.name, ' '*2))
             fingerings = cg_anglo_wheatstone_layout.get_fingerings(pitch)
             for f in fingerings:
-                print(indent(f.format(), ' '*4))
+                print(indent(str(f) if verbose else f.format(), ' '*4))
