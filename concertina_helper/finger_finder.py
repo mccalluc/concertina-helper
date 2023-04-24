274267a3e2997083be0a0233d3eaf0528fd96f80
mccalluc <mccalluc@users.noreply.github.com>
2023-04-24 12:07:41 -0400

    clean up logic in astar: Start from None node

--- concertina_helper/finger_finder.py
+++ concertina_helper/finger_finder.py
@@ -15,21 +15,28 @@ def find_best_fingerings(all_fingerings: list[set[AnnotatedBisonoricFingering]])
 @dataclass(frozen=True)
 class Node:
     position: int
-    annotated_fingering: AnnotatedBisonoricFingering
+    annotated_fingering: AnnotatedBisonoricFingering | None
 
 
 class FingerFinder(AStar):
     def __init__(self, fingerings: list[set[AnnotatedBisonoricFingering]]):
-        self.index = {
-            i: frozenset(Node(i, f) for f in f_set)
+        self.index: dict[int, set[Node]] = {
+            i: {Node(i, f) for f in f_set}
             for i, f_set in enumerate(fingerings)
         }
 
     def find(self):
-        start = list(self.index[0])[0]  # TODO: Add a None node at the start.
+        start = Node(-1, None)
         max_index = max(self.index.keys())
         goal = list(self.index[max_index])[0]
-        return [node.annotated_fingering for node in self.astar(start, goal)]
+        # is_goal_reached() only checks position,
+        # so I think we can use any final node.
+        # ... but then why is the goal parameter needed?
+
+        return [
+            node.annotated_fingering for node in self.astar(start, goal)
+            if node.annotated_fingering is not None
+        ]
 
     def heuristic_cost_estimate(self, current: Node, goal: Node) -> float:
         return goal.position - current.position
@@ -37,20 +44,21 @@ class FingerFinder(AStar):
     def distance_between(self, n1: Node, n2: Node) -> float:
         # TODO: Make the weightings here configurable.
         distance = abs(n1.position - n2.position)
-        f1 = n1.annotated_fingering.fingering
-        f2 = n2.annotated_fingering.fingering
-        if f1.direction != f2.direction:
-            # Maybe the cost of bellows change should depend on note length?
-            distance += 1
-        # TODO: Penalize finger shifts
-        # TODO: Penalize outer columns
-        # TODO: Penalize top row?
+        assert distance == 1  # Should only be used with immediate neighbors
+        if n1.annotated_fingering is not None and n2.annotated_fingering is not None:
+            f1 = n1.annotated_fingering.fingering
+            f2 = n2.annotated_fingering.fingering
+            if f1.direction != f2.direction:
+                # Maybe the cost of bellows change should depend on note length?
+                distance += 1
+            # TODO: Penalize finger shifts
+            # TODO: Penalize outer columns
+            # TODO: Penalize top row?
+            # TODO: Penalize pull at the start of a measure?
         return distance
 
     def neighbors(self, node) -> Iterable[Node]:
         return self.index[node.position + 1]
 
     def is_goal_reached(self, current: Node, goal: Node) -> bool:
-        # There could be multiple fingerings for the last note.
-        # They are all equally good.
         return current.position == goal.position
