319d8f4a5e7798282ccca883f204d9d2bafd9fd1
mccalluc <mccalluc@users.noreply.github.com>
2023-05-05 22:05:21 -0400

    test cli

--- concertina_helper/cli.py
+++ concertina_helper/cli.py
@@ -3,7 +3,7 @@
 import argparse
 from pathlib import Path
 from signal import signal, SIGPIPE, SIG_DFL
-from enum import auto, Enum
+from enum import Enum
 from collections.abc import Callable
 
 from pyabc2 import Tune
@@ -34,25 +34,26 @@ class Format(Enum):
         self.direction_f = direction_f
     # Enums are usually all caps, but these will come from the user.
     unicode = (
-        'Uses "○" and "●" to represent button state',
+        'uses "○" and "●" to represent button state',
         lambda pitch: '●',
         lambda pitch: '○',
         lambda direction: direction.name
     )
     ascii = (
-        'Uses "." and "@" to represent button state',
+        'uses "." and "@" to represent button state',
         lambda pitch: '@',
         lambda pitch: '.',
         lambda direction: direction.name
     )
     long = (
-        'Spells out the names of pressed buttons',
+        'spells out the names of pressed buttons',
         lambda pitch: str(pitch).ljust(4),
         lambda pitch: '--- ',
         lambda direction: direction.name
     )
 
-def _from_abc() -> None:  # pragma: no cover
+
+def _parse_and_print_fingerings() -> None:
     '''
     Parses command line arguments, finds optimal fingering for tune, and prints.
     '''
@@ -72,7 +73,7 @@ prints possible fingerings.
     parser.add_argument(
         '--format', choices=[f.name for f in Format],
         default=Format.long.name,
-        help='Output format. ' + ' / '.join(f'"{f.name}": {f.doc}' for f in Format))
+        help='Output format. ' + ' / '.join(f'"{f.name}" {f.doc}' for f in Format))
     parser.add_argument(
         '--layout_transpose', default=0, type=int, metavar='SEMITONES',
         help='Semitones to transpose the layout; Negative transposes down')
@@ -105,7 +106,6 @@ prints possible fingerings.
         if args.layout_path else
         load_bisonoric_layout_by_name(args.layout_name)
     ).transpose(args.layout_transpose)
-
     abc_str = args.abc_path.read_text()
     penalty_functions = [
         penalize_bellows_change(args.bellows_change_cost),
@@ -113,7 +113,8 @@ prints possible fingerings.
         penalize_pull_at_start_of_measure(args.pull_at_start_of_measure_cost)
     ]
     format = Format[args.format]
-    from_abc(
+
+    print_fingerings(
         abc_str, layout,
         button_down_f=format.button_down_f,
         button_up_f=format.button_up_f,
@@ -121,21 +122,23 @@ prints possible fingerings.
         penalty_functions=penalty_functions)
 
 
-def from_abc(
+def print_fingerings(
     abc_str: str,
     layout: BisonoricLayout,
     button_down_f: PitchToStr = lambda _: '@',
     button_up_f: PitchToStr = lambda _: '.',
     direction_f: Callable[[Direction], str] = lambda direction: direction.name,
     penalty_functions: list[PenaltyFunction] = []
-) -> None:  # pragma: no cover
+) -> None:
     '''
     The core of the CLI functionality.
     - `abc_str`: A multiline string containing ABC notation.
     - `layout`: A bisonoric layout, either built-in or supplied by user.
-    - `is_verbose`: Should the output show notes, or just whether buttons are down?
-    - `penalty_functions`: Encapsulate heuristics about what makes a good fingering.
+    - `button_down_f`, `button_up_f`, `direction_f`: Functions that determine output style.
+    - `penalty_functions`: Heuristic functions that define what makes a good fingering.
     '''
+    if not penalty_functions:
+        raise ValueError('At least one penalty function must be provided')
     tune = Tune(abc_str)
     t_l = TuneOnLayout(tune, layout)
 
---
+++ tests/test_cli.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+from unittest.mock import patch
+
+import pytest
+
+from concertina_helper.cli import (_parse_and_print_fingerings, print_fingerings)
+from concertina_helper.layouts.layout_loader import load_bisonoric_layout_by_name
+from concertina_helper.penalties import penalize_bellows_change
+
+
+def test_cli_help(capsys):  # pragma: no cover
+    try:
+        with patch('argparse._sys.argv', ['concertina-helper', '--help']):
+            _parse_and_print_fingerings()
+    except SystemExit:
+        pass
+    captured = capsys.readouterr().out
+    assert 'usage: concertina-helper' in captured
+    # We could read README.md and compare,
+    # but the linewrapping is different than when you call it from the shell
+
+
+def test_cli_missing_layout(capsys):   # pragma: no cover
+    try:
+        with patch('argparse._sys.argv', ['concertina-helper', 'fake']):
+            _parse_and_print_fingerings()
+    except SystemExit:
+        pass
+    captured = capsys.readouterr().err
+    assert '--layout_path --layout_name is required' in captured
+
+
+def test_cli_default_render(capsys):
+    with patch('argparse._sys.argv', ['concertina-helper', str(Path(__file__).parent / 'g-major.abc'), '--layout_name', '30_wheatstone_cg']):
+        _parse_and_print_fingerings()
+    captured = capsys.readouterr().out
+    assert 'Measure 1' in captured
+    assert '--- --- --- --- ---' in captured
+
+
+def test_cli_ascii_render(capsys):
+    with patch('argparse._sys.argv', ['concertina-helper', str(Path(__file__).parent / 'g-major.abc'), '--layout_name', '30_wheatstone_cg', '--format', 'ascii']):
+        _parse_and_print_fingerings()
+    captured = capsys.readouterr().out
+    assert 'Measure 1' in captured
+    assert '.....' in captured
+
+
+def test_cli_unicode_render(capsys):
+    with patch('argparse._sys.argv', ['concertina-helper', str(Path(__file__).parent / 'g-major.abc'), '--layout_name', '30_wheatstone_cg', '--format', 'unicode']):
+        _parse_and_print_fingerings()
+    captured = capsys.readouterr().out
+    assert 'Measure 1' in captured
+    assert '○○○○○' in captured
+
+
+def test_no_format_functions():
+    with pytest.raises(ValueError, match=r'one penalty function must be provided'):
+        print_fingerings(
+            'abc-not-read',
+            load_bisonoric_layout_by_name('30_wheatstone_cg'))
+
+
+abc_path = (Path(__file__).parent / 'g-major.abc').read_text()
+
+
+def test_default_render(capsys):
+    print_fingerings(
+        abc_path,
+        load_bisonoric_layout_by_name('30_wheatstone_cg'),
+        penalty_functions=[penalize_bellows_change(1)])
+    captured = capsys.readouterr().out
+    assert 'Measure 1' in captured
+    assert '.....' in captured
