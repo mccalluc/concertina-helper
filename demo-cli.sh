a006a3a1d979fe9a77d099df66097c586da898ef
mccalluc <mccalluc@users.noreply.github.com>
2023-05-20 11:31:56 -0400

    unstable shell tests

--- demo-cli.sh
+++ demo-cli.sh
@@ -3,17 +3,52 @@ set -o errexit
 
 die() { set +v; echo "$*" 1>&2 ; exit 1; }
 
-pip install flit
-flit install --symlink
-concertina-helper tests/g-major.abc \
-  --layout_name 30_wheatstone_cg \
-  --layout_transpose -2 \
-  --format long
+expect_equal() {
+  MESSAGE="$1"
+  shift
+  diff -c $@ || die "Expected $MESSAGE to be equal!"
+}
 
-perl -ne 'print if /usage:/../```/ and ! /```/' README.md > /tmp/expected.txt
-ACTUAL=/tmp/actual.txt
-concertina-helper --help > $ACTUAL
-diff /tmp/expected.txt $ACTUAL || die "Update CLI usage in README.md with $ACTUAL"
+# pip install flit
+# flit install --symlink
+
+expect_equal 'documentation' \
+<( perl -ne 'print if /usage:/../```/ and ! /```/' README.md ) \
+<( concertina-helper --help )
+
+# TODO: Non deterministic!
+
+# expect_equal 'defaults' \
+# <( concertina-helper tests/g-major.abc --layout_name 30_wheatstone_cg | head ) \
+# <( echo "Measure 1 - G4
+# PUSH:
+# --- --- --- --- ---    --- --- --- --- ---
+# --- --- --- --- ---    --- --- --- --- ---
+# --- --- G4  --- ---    --- --- --- --- ---
+# Measure 1 - A4
+# PUSH:
+# --- --- --- A4  ---    --- --- --- --- ---
+# --- --- --- --- ---    --- --- --- --- ---
+# --- --- --- --- ---    --- --- --- --- ---" )
+
+# expect_equal 'options' \
+# <( concertina-helper tests/g-major.abc \
+#    --layout_path concertina_helper/layouts/30_jefferies_cg.yaml \
+#    --layout_transpose 2 \
+#    --format ascii \
+#    --bellows_change_cost 3 \
+#    --finger_in_same_column_cost 6 | head ) \
+# <( echo "Measure 1 - G4
+# PULL:
+# .....   .....
+# ...@.   .....
+# .....   .....
+# Measure 1 - A4
+# PUSH:
+# .....   .....
+# .....   .....
+# ..@..   ....." )
+
 
 echo 'PASS!'
 
